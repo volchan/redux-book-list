0a9f3ff55e89ed519d6af8f0eb26667c41f36c1b
stephane delatorre <s.dlt@hotmail.fr>
2017-12-11 16:13:26 +0100

    session 4/50 done

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    {title: "The lord of the ring", pages: 400},
+    {title: "The game of thrones", pages: 820},
+    {title: "The Hobbit", pages: 850},
+    {title: "Harry Potter", pages: 350}
+  ];
+}
